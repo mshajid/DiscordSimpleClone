7a5b3fb36ff7d822f46d97fb418e8db3aaae7d1d
mshajid <70450142+mshajid@users.noreply.github.com>
2020-10-18 17:33:17 +0530

    Sidebar Voice Connected Styled

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,10 @@ import './Sidebar.css';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 import SidebarChannel from './SidebarChannel';
+import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CallIcon from '@material-ui/icons/Call';
+
 
 function Sidebar() {
     return (
@@ -27,6 +31,22 @@ function Sidebar() {
                 <SidebarChannel />
                 <SidebarChannel />
                 </div>
+            </div>
+
+            <div className="sidebar__voice">
+                <SignalCellularAltIcon 
+                className="sidebar__voiceIcon"
+                fontSize="large"
+                /> 
+            <div className="sidebar__voiceInfo">
+                <h3>Voice Connection</h3>
+                <p>Stream</p>
+            </div>
+
+            <div className="sidebar__voiceIcons">
+                <InfoOutlinedIcon />
+                <CallIcon />
+            </div>
             </div>
         </div>
     )
