4ecf6273c63a6e3b31f1ea2a238c4d118928c22f
mshajid <70450142+mshajid@users.noreply.github.com>
2020-10-23 17:36:39 +0530

    Login / Authentication

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Login.css';
 import {Button} from '@material-ui/core';
+import { auth, provider } from './firebase';
 
 function Login() {
     const signIn = () => {
+        auth.signInWithPopup(provider).catch((error) => alert(error.message));
 };
     return (
         <div className="login">
