6209f8d087d905f1572063fe3c2064c48b96619b
mshajid <70450142+mshajid@users.noreply.github.com>
2020-10-23 15:22:28 +0530

    Same - Selector hooks on App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import { selectUser } from "./features/userSlice";
+import Login from './Login';
+
 
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     //BEM Naming Convention CSS Architecture
     <div className="app">
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
